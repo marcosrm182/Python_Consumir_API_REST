d4ce50af0e67f519afb11971cd4987db5414cc8c
Marcos Rodriguez Morales <contacto@gorilast.com>
2017-12-04 20:29:33 +0100

    Cambio credenciales SESIONES

--- Consumir_API_REST/14_sesiones.py
+++ Consumir_API_REST/14_sesiones.py
@@ -7,12 +7,12 @@ if __name__== '__main__':
     url = 'https://api.github.com/user'
 
     session = requests.session()
-    session.auth = ( 'marcosrm182@gmail.com', 'riviera1985github')
+    session.auth = ( 'email@gmail.com', 'passwordGitHub')
 
     response = session.get(url)
 
     if response.ok:
-        response = session.get('https://github.com/marcosrm182')
+        response = session.get('https://github.com/usuarioGitHub')
         if response.ok:    
             print(response.content)
             print('...')
